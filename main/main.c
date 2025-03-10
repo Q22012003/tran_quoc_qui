66ef2a84f7c58c75c1b99dccc8224b2560961489
Q22012003 <tqui2024@gmail.com>
2025-03-10 00:48:37 -0700

    tính phương trình bậc 2

--- main/main.c
+++ main/main.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
-
+#include <math.h>
 void main() {
 
-    int a = 0, b = 0, c = 0;
+    float a = 0, b = 0, c = 0;
+    float delta = 0;
+    float x1=0, x2 = 0;
 
     printf("nhap a: ");
-    scanf_s("%d", &a);
-    printf("nhap b: ");
-    scanf_s("%d", &b);
-    printf("nhap c: ");
-    scanf_s("%d", &c);
+    scanf_s("%f", &a);
+    printf("nhap b:");
+    scanf_s("%f", &b);
+    printf("nhap c:");
+    scanf_s("%f", &c);
 
-    if (a > b && a > c)
+    if (a == 0)
     {
-        printf("max: %d", a);
-    }
-    else if (b > a && b > c)
-    {
-        printf("max: %d", b);
-    }
+        if (b == 0)
+            printf("phuong trinh vo nghiem");
+        else
+            printf("phuong trinh co 1 nghiem duy nhat: %f ", (-c / b));
+     }
+
     else
-    
-        printf("max: %d", c);
+      {
+        delta = b * b - 4 * a * c;
+        if (delta > 0) {
+            x1 = (-b + sqrt(delta)) / (2 * a);
+            x2 = (-b - sqrt(delta)) / (2 * a);
+            printf("x1=%f\n", x1);
+            printf("x2=%f\n", x2);
+        }
+        else if (delta == 0) {
+            x1 = (-b) / (2 * a);
+            printf("nghiem kep x= %f", x1);
+        }
+        else
+            printf("phuong trinh vo nghiem");
+      }
+
   
     
 }
