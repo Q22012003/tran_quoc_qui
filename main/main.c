87a7546e98dfbf3e75560c52916270155944217d
Q22012003 <tqui2024@gmail.com>
2025-03-10 08:58:22 -0700

    ATM

--- main/main.c
+++ main/main.c
@@ -1,38 +1,49 @@
 #include <stdio.h>
 
 void main() {
-    int Kwh = 0;
-    float tiendien = 0;
+    int n = 0;
+    int matkhau = 0;
+    int error = 0;
+    int money = 0;
+    switch (n)
+    {
+    case 0:
+    {
+        printf("moi ban nhap mat khau: ");
+        scanf_s("%d", &matkhau);
+        while (error < 3) {
+            if (matkhau == 1234) {
+                n=1;
+                break;
+            }
+            else {
+                error++;
+                if (error < 3) {
+                    printf("Sai mat khau! Moi ban nhap lai lan %d: ", error);
+                    scanf_s("%d", &matkhau);
+                }
+            }
+        }
+    }
+    
+    case 1:
+    {
+        do {
+            printf("moi ban nhap so tien can rut: ");
+            scanf_s("%d", &money);
+            if (money > 100000)
+            {
+                printf("so tien qua lon moi ban nhap lai\n");
+            }
+        } while (money > 100000);
+            n = 2;
+            break;
+        }
+    case 2:
+    {
+        printf("so tien ban can rut la: %d ", money);
+        break;
+    }
+    }
+    }
 
-    printf("So Kwh su dung: ");
-    scanf("%d", &Kwh);
-
-    if (Kwh >= 401) {
-        tiendien = (50 * 1.678) + (50 * 1.734) + (100 * 2.014) + (100 * 2.536) + (100 * 2.834) + ((Kwh - 400) * 2.927);
-        printf("Gia: 2.927 VND/1KWh\n");
-    }
-    else if (Kwh >= 301) {
-        tiendien = (50 * 1.678) + (50 * 1.734) + (100 * 2.014) + (100 * 2.536) + ((Kwh - 300) * 2.834);
-        printf("Gia: 2.834 VND/1KWh\n");
-    }
-    else if (Kwh >= 201) {
-        tiendien = (50 * 1.678) + (50 * 1.734) + (100 * 2.014) + ((Kwh - 200) * 2.536);
-        printf("Gia: 2.536 VND/1KWh\n");
-    }
-    else if (Kwh >= 101) {
-        tiendien = (50 * 1.678) + (50 * 1.734) + ((Kwh - 100) * 2.014);
-        printf("Gia: 2.014 VND/1KWh\n");
-    }
-    else if (Kwh >= 51) {
-        tiendien = (50 * 1.678) + ((Kwh - 50) * 1.734);
-        printf("Gia: 1.734 VND/1KWh\n");
-    }
-    else if (Kwh >= 0) {
-        tiendien = Kwh * 1.678;
-        printf("Gia: 1.678 VND/1KWh\n");
-    }
-    else {
-        printf("So Kwh khong hop le!\n");
-    }
-    printf("Tong tien: %.2f VND\n", tiendien);
-}
