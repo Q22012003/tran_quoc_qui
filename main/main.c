74969e6932163e8ac4603488d66af11120833ae7
Q22012003 <tqui2024@gmail.com>
2025-03-10 00:24:48 -0700

    viết chương trình tìm max của 3 số a,b,c

--- main/main.c
+++ main/main.c
@@ -2,16 +2,26 @@
 
 void main() {
 
-    int a = 0, b = 0;
+    int a = 0, b = 0, c = 0;
 
     printf("nhap a: ");
     scanf_s("%d", &a);
     printf("nhap b: ");
     scanf_s("%d", &b);
+    printf("nhap c: ");
+    scanf_s("%d", &c);
 
-    if (a > b)
-        printf("max: %a", &a);
+    if (a > b && a > c)
+    {
+        printf("max: %d", a);
+    }
+    else if (b > a && b > c)
+    {
+        printf("max: %d", b);
+    }
     else
-        printf("max: %b", &b);
-
+    
+        printf("max: %d", c);
+  
+    
 }
