3271d112636758a4eefca9751a87cf754c5030eb
Q22012003 <tqui2024@gmail.com>
2025-03-11 03:18:32 -0700

    tính lương thu nhập của nhân viên công ty

--- main/main.c
+++ main/main.c
@@ -1,49 +1,36 @@
 #include <stdio.h>
-
+#include <math.h>
 void main() {
-    int n = 0;
-    int matkhau = 0;
-    int error = 0;
-    int money = 0;
-    switch (n)
+    int luong = 0;
+    int thue = 0;
+    int luongchuan = 0;
+    printf("nhap luong: ");
+    scanf_s("%d", &luong);
+    if (luong < 0)
     {
-    case 0:
+        printf("Nhap du lieu sai");
+    }
+    else
     {
-        printf("moi ban nhap mat khau: ");
-        scanf_s("%d", &matkhau);
-        while (error < 3) {
-            if (matkhau == 1234) {
-                n=1;
-                break;
-            }
-            else {
-                error++;
-                if (error < 3) {
-                    printf("Sai mat khau! Moi ban nhap lai lan %d: ", error);
-                    scanf_s("%d", &matkhau);
-                }
-            }
+        if (luong < 10000000)
+        {
+            thue = luong * 0.1;
+            luongchuan = luong - thue;
+            printf("thue: %d\nluong chuan: %d", thue, luongchuan);
         }
-    }
-    
-    case 1:
-    {
-        do {
-            printf("moi ban nhap so tien can rut: ");
-            scanf_s("%d", &money);
-            if (money > 100000)
-            {
-                printf("so tien qua lon moi ban nhap lai\n");
-            }
-        } while (money > 100000);
-            n = 2;
-            break;
+        else if (luong >= 10000000&& luong<=15000000)
+        {
+            thue = luong * 0.15;
+            luongchuan = luong - thue;
+            printf("thue: %d\nluong chuan: %d", thue, luongchuan);
         }
-    case 2:
-    {
-        printf("so tien ban can rut la: %d ", money);
-        break;
-    }
-    }
+        else {
+            thue = luong * 0.2;
+            luongchuan = luong - thue;
+            printf("thue: %d\nluong chuan: %d", thue, luongchuan);
+        }
     }
 
+
+ }
+          
