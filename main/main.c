283f9508d478de31fe5750ee9908266385ff3bb3
Q22012003 <tqui2024@gmail.com>
2025-03-11 05:58:32 -0700

    bang cuu chuong

--- main/main.c
+++ main/main.c
@@ -1,36 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 void main() {
-    int luong = 0;
-    int thue = 0;
-    int luongchuan = 0;
-    printf("nhap luong: ");
-    scanf_s("%d", &luong);
-    if (luong < 0)
+    int tong = 0;
+    int i = 0;
+    int j = 0;
+    for ( i = 1; i<11 ; i++)
     {
-        printf("Nhap du lieu sai");
-    }
-    else
-    {
-        if (luong < 10000000)
+        for (j = 1; j < 11; j++)
         {
-            thue = luong * 0.1;
-            luongchuan = luong - thue;
-            printf("thue: %d\nluong chuan: %d", thue, luongchuan);
+            tong = i * j;
+            printf("%d x %d = %d\t", i, j, tong);
         }
-        else if (luong >= 10000000&& luong<=15000000)
-        {
-            thue = luong * 0.15;
-            luongchuan = luong - thue;
-            printf("thue: %d\nluong chuan: %d", thue, luongchuan);
-        }
-        else {
-            thue = luong * 0.2;
-            luongchuan = luong - thue;
-            printf("thue: %d\nluong chuan: %d", thue, luongchuan);
-        }
+        printf("\n");
     }
-
-
- }
+       
+    }
           
