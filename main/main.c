eaf6acc69012ee886b21e7cc52750043d9aeceb2
Q22012003 <tqui2024@gmail.com>
2025-03-13 05:52:14 -0700

    tinh tong phan tu trong mang

--- main/main.c
+++ main/main.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <math.h>
+
 void main() {
-    int a = 0;
-    int h = 0;
-    int i = 0;
-    double tong = 0;
+	int arr[10];
+	int i = 0;
+	int tong = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("\nnhap gia tri phan tu thu %d :", i);
+		scanf_s("%d", &arr[i]);
+		tong += arr[i];
+	}
 
-    printf("moi ban nhap chieu dai a : ");
-    scanf_s("%d", &a);
-    printf("moi ban nhap chieu cao h :");
-    scanf_s("%d", &h);
-
-    for (i = 0; i <= 1000; i++)
-    {
-        double x1 = pow((a + i * h), 2);
-        double x2 = pow((a + (i + 1) * h), 2);
-        tong += ((x1 + x2) * h) / 2;
-    }
-    printf("gia tri la: %f", tong);
+	printf("\ntong i: %d", tong);
 }
-
-    
-
-
-
-          
