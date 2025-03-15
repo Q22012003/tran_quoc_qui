21db8b55e1aa59b229a4cddc7e723bd4d28fa4cc
Q22012003 <tqui2024@gmail.com>
2025-03-14 20:25:53 -0700

    bài tập 3: nhập sai 5 lần thì out (vonglap.txt)

--- main/main.c
+++ main/main.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 
 void main() {
-	int n = 0;
+	int n;
 	int i = 0;
-	int tong = 1;
-	printf("moi ban nhap 1 so:");
-	scanf_s("%d", &n);
+	
 
-	for (i = 1; i <= n; i++)
-	{
-		tong *= i;
-	}
-	printf("gia tri giai thua: %d", tong);
+	do {
+		printf("\nmoi ban nhap so 0: ");
+		scanf_s("%d", &n);
+		if (n != 0)
+		{
+			i++;
+		}
+		printf("nhap sai lan thu %d: ",i);
+	} while (n != 0&&i<5);
 }
