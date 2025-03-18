5e27270bda243a7dace8ea3ef2a2f00f8d81a4aa
Q22012003 <tqui2024@gmail.com>
2025-03-18 02:31:22 -0700

    Bài 9: tăng dần các phần tử trong mảng

--- main/main.c
+++ main/main.c
@@ -6,23 +6,39 @@ void main()
 	int i = 0;
 	int max = 0;
 	int size = sizeof(A)/sizeof(A[0]);
+	int j = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		printf("nhap phan tu thu %d: ", i);
+		printf("\nnhap phan tu thu %d: ", i);
 		scanf_s("%d", &A[i]);
 	}
 	for (i = 0; i < size; i++)
 	{
-		printf("\t%d ", A[i]);
+		printf("\t%d", A[i]);
+	
 	}
-	int temp = A[0];
-		A[0] = A[1];
-		A[1] = temp;
-		for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
+	{
+		int min_index = i;
+		for (j = i + 1; j < size; j++)
 		{
+			if (A[j] < A[min_index])
+			{
+				min_index = j;
+			}
+			int temp = A[i];
+			A[i] = A[min_index];
+			A[min_index] = temp;
 
-			printf("%d", A[i]);
 		}
+	}
+	printf("\n");
+	for (i = 0; i < size; i++)
+	{
+		printf("\t%d", A[i]);
+
+	}
+	
 
 }
