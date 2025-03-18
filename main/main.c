55422ac66ba062b13b554a536a3818cb56587610
Q22012003 <tqui2024@gmail.com>
2025-03-18 01:20:46 -0700

    bài 6: In ra các giá trị lẻ trong mảng

--- main/main.c
+++ main/main.c
@@ -14,13 +14,10 @@ void main()
 	}
 	for (i = 0; i < 10; i++)
 	{
-		if (A[i] < 0)
+		if (A[i]%2==1)
 		{
-			A[i] = 0;
+			printf("%d\t", A[i]);
 		}
 	}
-	for (i = 0; i < 10; i++)
-	{
-		printf("\t%d", A[i]);
-	}
+	
 }
