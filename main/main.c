98cf3f37055f34a6fcaac9a1ef3ceecef4fa04dc
Q22012003 <tqui2024@gmail.com>
2025-03-18 02:45:06 -0700

    Bài 2: đếm ký tự space

--- main/main.c
+++ main/main.c
@@ -4,13 +4,17 @@ void main()
 {
 	char str[100];
     int size = sizeof(str) / sizeof(str[0]);
-    int count = 0;
+    int count = 0, i = 0;
 
 printf("nhap chuoi: ");
 gets(str);
 
-while (str[count] != '\0') {
-    count++;
+while (str[i] != '\0') {
+    if (str[i] == ' ')
+    {
+        count++;
+    }
+    i++;
 }
-printf("so ky tu %d", count);
+printf("so ky tu ' ' %d", count);
 }
