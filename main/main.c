3d5428f46b7a2b9824740123651f3b7bc9bd1dad
Q22012003 <tqui2024@gmail.com>
2025-03-18 02:01:54 -0700

    Bài 8: Hàm hoán vị 2 số nguyên

--- main/main.c
+++ main/main.c
@@ -5,26 +5,24 @@ void main()
 	int A[10];
 	int i = 0;
 	int max = 0;
-	int n = 0;
+	int size = sizeof(A)/sizeof(A[0]);
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("nhap phan tu thu %d: ", i);
 		scanf_s("%d", &A[i]);
 	}
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < size; i++)
 	{
-		if (A[i] > max)
-		{
-			max = A[i];
-		}
+		printf("\t%d ", A[i]);
 	}
-	for (i = 0; i < 10; i++)
-	{
-		if (A[i] == max)
+	int temp = A[0];
+		A[0] = A[1];
+		A[1] = temp;
+		for (i = 0; i < size; i++)
 		{
-			printf("\n%d vi tri nay la max", i);
-			printf("\n %d la so lon nhat trong mang", A[i]);
+
+			printf("%d", A[i]);
 		}
-	}
+
 }
