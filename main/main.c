0e28b87df7554878f487e261370af6bdde93244a
Q22012003 <tqui2024@gmail.com>
2025-03-20 05:15:14 -0700

    bài tập thêm

--- main/main.c
+++ main/main.c
@@ -1,27 +1,64 @@
-﻿#include <stdio.h>
+﻿#include<stdio.h>
+#define MAX 10
 
-int main() {
-    char str[100000];
-    int inWord = 0;
+//chức năng: nhập mảng
+//Input: không
+//OUTPUT: không
+void InputArray(int *A,int size) {
+	int i = 0;
 
-  
-    printf("Nhap chuoi: ");
-    gets(str);
+	for (i = 0; i < size; i++)
+	{
+		printf("nhap gia tri %d:  ",i);
+		scanf_s("%d", (A+i));
+	}
+}
 
-   
-    for (int i = 0; str[i] != '\0'; i++) {
-        
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r' || str[i] == '\f' || str[i] == '\v') {
-            inWord = 0; 
-        }
-        else if (inWord == 0) { 
-            if (str[i] >= 'a' && str[i] <= 'z') { 
-                str[i] = str[i] - ('a' - 'A'); 
-            }
-            inWord = 1; 
-        }
-    }
+void OutputArray(int *A, int size) {
+	int i = 0;
+	for (i = 0; i < size; i++) {
+		printf("\t%d", *(A+i));
+	}
 
-    printf("Chuoi sau khi chinh sua: %s\n", str);
-    return 0;
+}
+
+//ham so sanh tang dan trong mang
+//input:mangA , so luong phan tu trong mang A
+//output: no
+void Tangdan(int *A, int size)
+{
+	int i, j = 0;
+	for (i = 0; i < size; i++) {
+		int *min_index = A+i;
+		for (j = i + 1; j < size; j++) {
+			if (*(A+j) < *min_index) {
+				min_index = A+j;
+			}
+		}
+		int temp = *(A+i);
+		*(A+i) = *min_index;
+		*min_index = temp;
+
+	}
+}
+
+void tachten_holot(char*B, int size) {
+	while (*B != 0) {
+		for (int i = 0; i < size; i++)
+		{
+			if (*(B+i) == ' ') {
+
+			 }
+		}
+	}
+}
+
+void main() {
+	int A[MAX];
+	int sizeA = sizeof(A) / sizeof(A[0]);
+	char B[] = "TRAN QUOC QUI";
+	int sizeB = sizeof(B) / sizeof(B[0]);
+	InputArray(A,10);
+	Tangdan(A,sizeA);
+	OutputArray(A,10);
 }
