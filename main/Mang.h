e72dbe017cfcefe2004b515fc53be1ca012be745
Q22012003 <tqui2024@gmail.com>
2025-03-23 23:12:27 -0700

    Bài 6,7,8,9: xây dựng hàm : _ in ra số nguyên tố                                             _giá trị lớn nhất                                             _hoan vi                                             _ sap xep mang tang dan

--- main/Mang.h
+++ main/Mang.h
@@ -1,2 +1,6 @@
 int tong_arr(int mang[], int so);
 void Value_le_arr(int mang[], int so);
+void so_nguyen_to(int mang[], int so);
+void so_lon_nhat_arr(int mang[], int so);
+void hoan_vi(int a, int b);
+void mang_tang_dan(int mang[], int so);
--- main/main.c
+++ main/main.c
@@ -4,7 +4,15 @@
 
 void main() {
 	int A[10] = {1,2,3,4,5,6,7,8,9,10};
+	int a = 1;
+	int b = 2;
+
 	printf("tong mang: %d", tong_arr(A, 10));
 	printf("\n");
 	Value_le_arr(A, 10);
+	so_nguyen_to(A, 10);
+	so_lon_nhat_arr(A, 10);
+	hoan_vi(a, b);
+	mang_tang_dan(A, 10);
+
 }
