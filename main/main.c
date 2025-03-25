6e5148ffd9054cecd84a922db44ce2424966ac20
Q22012003 <tqui2024@gmail.com>
2025-03-25 06:59:10 -0700

    khai bao hoc sinh

--- main/main.c
+++ main/main.c
@@ -1,73 +1,49 @@
 ﻿#include<stdio.h>
-#include<math.h>
-struct phan_so
-{
-	int tu;
-	int mau;
-};
 
 typedef struct {
-	int tu;
-	int mau;
-}phan_so_t;
+	char* ten;
+	int tuoi;
+	float diem_toan;
+	float diem_van;
+	float diem_trung_binh;
+	float xep_loai;
+}HOC_SINH;
 
-
-//chuc nang : nhan 2 phan so
-//input : 
- //struct phan_so x - phan so thu nhat
-// struct phan_so y - phan so thu hai
-phan_so_t phan_so (phan_so_t A, phan_so_t  B)
+HOC_SINH xeploai(HOC_SINH* a, int so_luong)
 {
-	phan_so_t phan_so Q;
-	Q.tu = A.tu * B.tu;
-	Q.mau = A.mau * B.mau;
-
-	return Q;
 	
-
-}
-
-//chuc nang: tim phan so lon nhat trong mang phan so
-//input:
-// phan_so_t* mang - dia chi bat dau cua mang chua cac phan so
-// int so_luong - so luong phan tu trong mang
-//output : (phan_so_t) phan so co gia tri lon nhat duoc tim thay
-
-phan_so_t phan_so_lon_nhat(phan_so_t* mang, int a) {
-	int i = 0;
-	float ketua
-
-	for (i = 0; i < a; i++)
+	for (int i = 0; i < so_luong; i++)
 	{
-		if ()
+		float diem_trung_binh = ((float) a[i].diem_toan + (float)a[i].diem_van)/ 2.0;
+		if (diem_trung_binh < 5)
 		{
-
+			printf("HOC SInh yeu");
+		}
+		else if (diem_trung_binh > 5)
+		{
+			printf("HOC SINH TRUNG BINH");
+		}
+		else if (diem_trung_binh > 6.5)
+		{
+			printf("HOC SINH kha");
+		}
+		else
+		{
+			printf("hoc sinh gioi");
 		}
 	}
 }
 
-void main() {
-	phan_so_t phan_so A;
-	phan_so_t phan_so B;
-	phan_so_t mang[] = {
-		{1,2},
-		{7,4},
-		{9,6},
-		{1,8},
-		{10,9},
+
+
+void main()
+{
+	HOC_SINH a[] = {
+		{.ten = "Nguyen Van A",.tuoi = 19,.diem_toan = 8,.diem_van = 2},
+		{.ten = "Nguyen Van B",.tuoi = 19,.diem_toan = 9,.diem_van = 1},
+		{.ten = "Nguyen Van C",.tuoi = 19,.diem_toan = 9,.diem_van = 3},
+		{.ten = "Nguyen Van D",.tuoi = 19,.diem_toan = 9,.diem_van = 4},
+		{.ten = "Nguyen Van E",.tuoi = 19,.diem_toan = 10,.diem_van = 5},
 	};
-
-	//A.tu = 1;
-	//A.mau = 2;
-
-	//B.tu = 1;
-	//B.mau = 3;
-	//phan_so_t ketqua = phan_so(A, B);
-
-	//phan_so_t = { 1,2 };
-	//phan_so_t = { .mau = 3, .tu = 2 };
-	//printf("%d", ketqua.tu);
-	//printf("\n%d", ketqua.mau);
-
-	
+		xeploai(a,5);
 }
