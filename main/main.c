c4a8de2796a2f29a9be2c1416b510e48811f5dcb
Q22012003 <tqui2024@gmail.com>
2025-03-27 00:44:30 -0700

    bài 2: viết hàm nhân 2 phân số

--- main/main.c
+++ main/main.c
@@ -4,15 +4,27 @@ struct phan_so {
 	int tu;
 	int mau;
 };
-
 typedef struct phan_so phanso;
+
+ phanso tinh_phanso(phanso a, phanso b) {
+	phanso tich = { (a.tu * b.tu),(a.mau * b.mau) };
+	return tich;
+}
+
 
 void main()
 {
-	phanso a;
-	printf("nhap tu: ");
-	scanf_s("%d", &a.tu);
-	printf("nhap mau :");
-	scanf_s("%d", &a.mau);
+	phanso a = { 2,3 };
+	phanso b = { 3,4 };
+    
+	phanso tong = tinh_phanso(a, b);
+
+	printf("gia tri tu cua ham la: %d", tong.tu);
+	printf("\ngia tri mau cua ham la: %d", tong.mau);
+
+
+
+	
+	
 
 }
