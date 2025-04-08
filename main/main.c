b817ea1135a6fd070bf24ca56c635c688d459449
Q22012003 <tqui2024@gmail.com>
2025-04-08 07:13:35 -0700

    boc xep value

--- main/main.c
+++ main/main.c
@@ -1,84 +1,24 @@
-﻿#include <stdio.h>
+﻿#include<stdio.h>
 
-typedef enum
+
+typedef union {
+	unsigned short x;
+	unsigned char y[2];
+}union_t;
+
+void main()
 {
-	NAM,
-	NU
-} gioiTinh_t;
+	unsigned short val = 0x1234;
+	unsigned char low = 0x34;
+	unsigned char high = 0x12;
 
-typedef enum
-{
-	GIOI,
-	KHA,
-	TRUNG_BINH,
-	YEU
-} loai_t;
+	union_t sv;
+	sv.x = val;
+	high=sv.
+	sv.y_low = low;
 
-typedef struct
-{
-	char* ten;
-	int tuoi;
-	gioiTinh_t gioi_tinh;
-	float diem_toan;
-	float diem_van;
-	loai_t loai;
-} hoc_sinh_t;
+	printf("0x%s", sv.y_high);
+	printf("\n0x%s", sv.y_low);
 
-void xeploai(hoc_sinh_t* sv, int size)
-{
-	float diem_trung_binh;
-	for (int i = 0; i < size; i++)
-	{
-		diem_trung_binh = (sv[i].diem_toan + sv[i].diem_van) / 2.0;
-		if (diem_trung_binh < 5.0)
-		{
-			sv[i].loai = YEU;
-		}
-		else if (diem_trung_binh < 6.5)
-		{
-			sv[i].loai = TRUNG_BINH;
-		}
-		else if (diem_trung_binh < 8.0)
-		{
-			sv[i].loai = KHA;
-		}
-		else
-		{
-			sv[i].loai = GIOI;
-		}
-	}
-}
 
-void in_thong_tin(hoc_sinh_t* sv, int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		printf("Ten: %s, Tuoi: %d, Gioi tinh: %s, Toan: %.1f, Van: %.1f, Xep loai: ",
-			sv[i].ten, sv[i].tuoi,
-			sv[i].gioi_tinh == NAM ? "Nam" : "Nữ",
-			sv[i].diem_toan, sv[i].diem_van);
-
-		/*switch (sv[i].loai)
-		{
-		case GIOI: printf("Giỏi\n"); break;
-		case KHA: printf("Khá\n"); break;
-		case TRUNG_BINH: printf("Trung Bình\n"); break;
-		case YEU: printf("Yếu\n"); break;
-		}*/
-	}
-}
-
-int main()
-{
-	hoc_sinh_t arr[] = {
-		{"Tommy", 18, NAM, 5, 5},
-		{"Mono", 18, NU, 8, 7}
-	};
-
-	int size = sizeof(arr) / sizeof(arr[0]);
-
-	xeploai(arr, size);
-	in_thong_tin(arr, size);
-
-	return 0;
 }
