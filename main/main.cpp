4348256f2424d53c531e9de668e24cef3d25ec09
Q22012003 <tqui2024@gmail.com>
2025-05-24 20:45:50 -0700

    Bài 2: viết hàm có đối số là số nguyên 16bit và sử dụng printf in ra màn hình 2 số gồm 8bit cao và 8 bit thấp

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,22 @@
-#include<stdio.h>
-#include <stdlib.h>
-#include"Library.h"
+#include <stdio.h>
+#include<stdint.h>
 
-
-
+typedef union {
+    uint16_t x;
+    struct {
+        uint8_t low;
+        uint8_t high;
+    }split_8bit;
+}split_16bit;
 
 int main()
 {
-   
+    split_16bit data;
+    printf("nhap 16bit: ");
+    scanf_s("%hu", &data.x);
+
+    printf("8 bit cao (high): %u (0x%02X)\n", data.split_8bit.high, data.split_8bit.high);
+    printf("8 bit thap (low): %u (0x%02X)\n", data.split_8bit.low, data.split_8bit.low);
+
+    return 0;
 }
