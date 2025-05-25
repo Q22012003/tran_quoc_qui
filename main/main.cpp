6b9f21ad14d614d05c0ef2a75356091671400450
Q22012003 <tqui2024@gmail.com>
2025-05-24 20:46:41 -0700

    Bài 3: xây dựng Class giải phương trình bậc 2

--- main/main.cpp
+++ main/main.cpp
@@ -1,22 +1,47 @@
-#include <stdio.h>
-#include<stdint.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-typedef union {
-    uint16_t x;
-    struct {
-        uint8_t low;
-        uint8_t high;
-    }split_8bit;
-}split_16bit;
+class PhuongTrinhBac2 {
+private:
+    float a, b, c;
 
-int main()
-{
-    split_16bit data;
-    printf("nhap 16bit: ");
-    scanf_s("%hu", &data.x);
+public:
+    PhuongTrinhBac2(float a, float b, float c) : a(a), b(b), c(c) {}
 
-    printf("8 bit cao (high): %u (0x%02X)\n", data.split_8bit.high, data.split_8bit.high);
-    printf("8 bit thap (low): %u (0x%02X)\n", data.split_8bit.low, data.split_8bit.low);
+    void tinhtoan() {
+        float delta = b * b - 4 * a * c;
+
+        if (a == 0) {
+            cout << "Day khong phai phuong trinh bac 2." << endl;
+        }
+        else if (delta < 0) {
+            cout << "Phuong trinh vo nghiem." << endl;
+        }
+        else if (delta == 0) {
+            cout << "Phuong trinh co nghiem kep: x = " << -b / (2 * a) << endl;
+        }
+        else {
+            float x1 = (-b + sqrt(delta)) / (2 * a);
+            float x2 = (-b - sqrt(delta)) / (2 * a);
+            cout << "Phuong trinh co 2 nghiem phan biet:" << endl;
+            cout << "x1 = " << x1 << ", x2 = " << x2 << endl;
+        }
+    }
+};
+
+int main() {
+    float a, b, c;
+
+    cout << "Nhap he so a: ";
+    cin >> a;
+    cout << "Nhap he so b: ";
+    cin >> b;
+    cout << "Nhap he so c: ";
+    cin >> c;
+
+    PhuongTrinhBac2 pt(a, b, c);
+    pt.tinhtoan();
 
     return 0;
 }
